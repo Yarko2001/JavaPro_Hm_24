4ae7c487abe35a1ee57420c81870e2ae6474e129
Degtiar Yaroslav <merrynan2001@gmail.com>
2023-02-09 17:59:36 +0300

    init commit

---
+++ src/test/java/dehtiar/hillel/ua/javapro_hm_24/JavaProHm24ApplicationTests.java
@@ -0,0 +1,13 @@
+package dehtiar.hillel.ua.javapro_hm_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProHm24ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
